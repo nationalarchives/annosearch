92b4d080b8d9bf4fb3a14630ac15395809a53433
John Moore <jmoore@zedstar.org>
2024-11-01 16:12:23 +0000

    refactor

--- src/AnnoSearch.ts
+++ src/AnnoSearch.ts
@@ -1,45 +1,39 @@
-import { timeStamp } from 'console';
 import { search as searchFunction } from './search';
 
 class AnnoSearch {
-  
-  private maxHits: number;
-  private port: number;
-  private host: string;
+    constructor(
+        private maxHits: number = 10,
+        private port: number = 3000,
+        private host: string = 'http://localhost'
+    ) { }
 
-  constructor(maxHits: number = 10, port: number = 3000, host: string = 'http://localhost') {
-    this.maxHits = maxHits;
-    this.port = port; 
-    this.host = host;
-  }
+    setHost(host: string) {
+        this.host = host;
+    }
 
-  setHost(host: string) {
-    this.host = host;
-  }
+    getHost(): string {
+        return this.host;
+    }
 
-  getHost(): string {
-    return this.host;
-  }
+    setPort(port: number) {
+        this.port = port;
+    }
 
-  setPort(port: number) {
-    this.port = port;
-  }
+    getPort(): number {
+        return this.port;
+    }
 
-  getPort(): number {
-    return this.port;
-  }
+    setMaxHits(maxHits: number) {
+        this.maxHits = maxHits;
+    }
 
-  setMaxHits(maxHits: number) {
-    this.maxHits = maxHits;
-  }
+    getMaxHits(): number {
+        return this.maxHits;
+    }
 
-  getMaxHits(): number {
-    return this.maxHits;
-  }
-
-  async search(indexId: string, query: string, startOffset: number) {
-    return await searchFunction(indexId, query, this.maxHits, startOffset);
-  }
+    async search(indexId: string, query: string, startOffset: number) {
+        return await searchFunction(indexId, query, this.maxHits, startOffset);
+    }
 }
 
 export default AnnoSearch;
